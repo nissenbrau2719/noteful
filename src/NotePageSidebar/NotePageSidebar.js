1264f45897232c804074bcfedcf834799e112c94
Paul Nissen <nissenbrau@gmail.com>
2020-02-12 20:45:02 -0700

    added functionality to Go Back button on NotePageSidebar

--- src/NotePageSidebar/NotePageSidebar.js
+++ src/NotePageSidebar/NotePageSidebar.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import './NotePageSidebar.css';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "./NotePageSidebar.css";
 
 function NotePageSidebar(props) {
   return (
-    <div className='noteSidebar'>
-      <button className='goBackBtn'>Go back</button>
-      <h2 className='noteFolder'>{props.folderName}</h2> 
+    <div className="noteSidebar">
+      <button className="goBackBtn" onClick={() => props.history.goBack()}>
+        Go back
+      </button>
+      <h2 className="noteFolder">{props.folderName}</h2>
     </div>
-  )
+  );
 }
 
 export default withRouter(NotePageSidebar);
