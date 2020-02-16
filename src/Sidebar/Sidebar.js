2284f61a91ca154bf66619516047a5b484c5bfd2
Paul Nissen <nissenbrau@gmail.com>
2020-02-15 17:04:43 -0700

    added note count to folder tabs

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -7,6 +7,8 @@ class Sidebar extends React.Component {
   static contextType = NotefulContext
   render() {
     const folderList = this.context.folders.map(folder => {
+      const noteCount =  this.context.notes.filter(note => note.folderId === folder.id).length
+      
       return (
         <li key={folder.id} className="folderItem">
           <NavLink
@@ -16,8 +18,9 @@ class Sidebar extends React.Component {
             id={folder.id}
             className="folderLink"
           >
-            {folder.name}
+            {folder.name}<span className="count">{noteCount}</span>
           </NavLink>
+          
         </li>
       );
     });
